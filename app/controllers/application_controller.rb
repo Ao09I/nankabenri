3e9b07418a95fcf86350fc756aae76241ef00b4c
Ao09I <skmaoi.000@icloud.com>
2022-12-14 03:00:00 +0900

    CSSづけ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+    before_action :get_current_user
+    def get_current_user
+        @current_user = User.find_by("#{session[:user_id]}")
+    end
 end
