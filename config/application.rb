c0fb3aff27ff7b5a51a8d926936b0d97ee8f833b
Ao09I <skmaoi.000@icloud.com>
2023-01-12 12:55:21 +0900

    calendar_controller fixed

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module Nankabenri
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.hosts << 'nankabenri.onrender.com'
   end
 end
