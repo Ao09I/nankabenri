ae115ea3734b5af2eb6d908394a02cd04d94ab21
Ao09I <skmaoi.000@icloud.com>
2023-01-18 20:19:22 +0900

    日付取得変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,8 @@ class Item < ApplicationRecord
     validates :tweet_id, {presence: true, uniqueness: true}
 
     #正規表現を定数に入れる
-    DATE_FORMAT = /(0?[1-9]|1[0-2])[\/](0?[1-9]|[12][0-9]|3[01])/
+    DATE_FORMAT = /(\d{1}|\d{2})\/(\d{2}|\d{1})/
 end
+
+
+#(0?[1-9]|1[0-2])[\/](0?[1-9]|[12][0-9]|3[01])
